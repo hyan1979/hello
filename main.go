d134388e35e70686cba0cd78a3f367c65dcdfd2d
Taekyung Lee <taekyung.lee@atto-research.com>
2016-06-25 12:08:38 +0900

    the second commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	//typesize()
+	//enum()
+	//fmt.Println(x)
+	//scope()
+	//scanf()
+	//pointer()
+	//average()
+	//multi_return()
+	//first_class()
+	//anonymous_func()
+	//vertex_method()
+	//entity_method()
+	//getArea()
+	//call_fibonacci()
+	//call_bicycle()
+//	call_read()
+//	call_http()
+	call_pingpong()
+}
